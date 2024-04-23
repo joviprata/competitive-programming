438c46c1cb8eb2bdec00c0be53402d4df32635a0
Jovi Prata <115483518+joviprata@users.noreply.github.com>
2024-04-23 11:57:52 -0300

    Update code_template_no_commentary.cpp

--- cpp-codes/code_template_no_commentary.cpp
+++ cpp-codes/code_template_no_commentary.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define sws ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
 #define pb push_back
 #define all(x) x.begin(), x.end()
-#define pii pair <int, int>
-#define sws ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef pii pair <int, int>
+typedef vi vector <int>
 
 int32_t main() {
     sws;
 
+    
     return 0;
 }
