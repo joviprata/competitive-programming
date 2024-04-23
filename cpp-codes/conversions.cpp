946b5736b70772e968e550e40b26ac1f93979973
Jovi Prata <115483518+joviprata@users.noreply.github.com>
2024-04-23 13:03:26 -0300

    Create conversions.cpp

---
+++ cpp-codes/conversions.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+    string num_str;
+    int num_int = stoi(str_num);
+    float num_float = stof(str_num);
+    double num_double = stod(str_num);
+
+    string num_str_2 = to_string(num_int);
+}
