1a1797fe2ca1b56665e4e1235c49271b5a502cf7
Jovi Prata <115483518+joviprata@users.noreply.github.com>
2024-04-23 19:23:08 -0300

    Update code_template_no_commentary.cpp

--- cpp-codes/code_template_no_commentary.cpp
+++ cpp-codes/code_template_no_commentary.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define sws ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long
+#define endl "\n"
 #define pii pair <int, int>
 #define mp make_pair
 #define ff first
