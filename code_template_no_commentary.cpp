64983d2a1a1e74aa764f132ac3aa5d8729798b56
Jovi Prata <115483518+joviprata@users.noreply.github.com>
2024-04-23 11:34:29 -0300

    Add files via upload

---
+++ code_template_no_commentary.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair <int, int>
+#define sws ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int32_t main() {
+    sws;
+}
